5dc87e13d202ffc51986bffb54451ad2a3f4da12
Luke Short <ekultails@gmail.com>
2024-06-23 21:22:04 -0600

    [main] Add YAML configuration
    Using the Serde YAML library. This project has been abandoned
    recently so we will move to something else long-term.

--- src/main.rs
+++ src/main.rs
@@ -1,14 +1,34 @@
-#[derive(Debug)]
+use serde::{Deserialize, Serialize};
+use serde_yaml::{self};
+
+#[derive(Debug, Deserialize, Serialize)]
 struct LightsData {
     preview_character: char,
     color: String,
     lights_on: [i8; 8],
 }
 
+#[derive(Debug, Serialize, Deserialize)]
+struct YamlConfig {
+    audio_output_device: i8,
+    pins_layout: Vec<i8>,
+    latency: f32,
+    lights: Vec<Vec<LightsData>>,
+}
 
 impl Default for LightsData {
     fn default () -> LightsData {
         LightsData{preview_character: '*', color: String::from("white"), lights_on: [1,1,1,1,1,1,1,1]}
+    }
+}
+
+impl Default for YamlConfig {
+    fn default () -> YamlConfig {
+        YamlConfig {
+            audio_output_device: 0,
+            pins_layout: vec![8,9,7,0,2,3,12,13],
+            latency: 0.0
+        }
     }
 }
 
@@ -23,7 +43,6 @@ fn lights_preview(all_lights: Vec<LightsData>) {
 
 fn main() {
     let lights_data_default = LightsData::default();
-    let number_of_lights = 1;
     let mut all_lights: Vec<LightsData> = Vec::new();
     all_lights.push(lights_data_default);
     lights_preview(all_lights);
